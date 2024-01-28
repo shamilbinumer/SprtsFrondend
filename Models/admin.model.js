8a94baaf7b1dced09ff40468a12f8a7de16cef34
shamil <shamilmohd418@gmail.com>
2024-01-28 12:26:57 +0530

    sec

--- Models/admin.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const admin_schema=new mongoose.Schema({
-    username:{type:String},
-    email:{type:String},
-    phone:{type:String},
-    password:{type:String}
-})
-
-export default mongoose.model.admins||mongoose.model("admin",admin_schema)
